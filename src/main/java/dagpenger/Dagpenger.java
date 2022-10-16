fc51a25f672c09ff6099fd8edc86f34c8a0db20a
eline00 <90403068+eline00@users.noreply.github.com>
2022-10-16 14:04:25 +0200

    Rettet opp i feil i koden. Nå gir den riktig utskrift.

--- src/main/java/dagpenger/Dagpenger.java
+++ src/main/java/dagpenger/Dagpenger.java
@@ -1,12 +1,11 @@
 package dagpenger;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Dagpenger {
 
-    private double income1 = 0,
-            income2 = 0,
-            income3 = 0;       // inntekter per kalenderårene
+    private double income1, income2, income3;       // inntekter per kalenderårene
 
     private final int G = 111477;     // Grunnbeløp per 1.mai 2022
     private final int workdaysPerYear = 260;        // antall arbeidsdager i året per NAVs definisjon
@@ -24,25 +23,27 @@ public class Dagpenger {
             return false;
         } else {
             // enten tjent over 3G siste 3 årene eller tjent over 1.5G forrige kalenderår
-            return !(totalIncome < 3 * G) && !(income2 < 1.5 * G);
+            return !(totalIncome < 3 * G) && !(income3 < 1.5 * G);
         }
     }
 
     public int dailyRate() {
         double dailyAllowanceBasis;
-        double avarageIncome = income1+income2+income3/3;       // gjennomsnittlig inntekt de siste 3 kalederårene
-        // dagpengegrunnlaget er høyeste verdi av enten intekten siste året, eller gjennomsnittet av de 3 siste årene
+        double avarageIncome = (income1+income2+income3)/3;       // gjennomsnittlig inntekt de siste 3 kalederårene
+
         if (income3 > avarageIncome) {
             dailyAllowanceBasis= income3;
         } else {
             dailyAllowanceBasis = avarageIncome;
         }
+
         return (int) dailyAllowanceBasis/workdaysPerYear;   // dagsats = dagpengegrunnlag/arbeidsdager per år rundet opp
     }
 
     @Override
     public String toString() {
-        String s = "";
+        String s;
+
         if (qualified()) {
             s = "Du er kvalifisert til å få dagpenger. \nDin dagsats er på: " + dailyRate() + "kr.";
         } else {
@@ -54,11 +55,11 @@ public class Dagpenger {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter first income- ");
+        System.out.print("Din årsintekt i 2019- ");
         int a = sc.nextInt();
-        System.out.print("Enter second income- ");
+        System.out.print("Din årsintekt i 2020- ");
         int b = sc.nextInt();
-        System.out.print("Enter third income- ");
+        System.out.print("Din årsintekt i 2021- ");
         int c = sc.nextInt();
 
         Dagpenger dagpenger = new Dagpenger(a, b, c);
